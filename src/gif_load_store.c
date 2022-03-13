62ef2e46000a6c659bc93f9868cf9cb98aa1de30
Macdu <Macdu@users.noreply.github.com>
2022-03-13 21:39:22 +0100

    Export speed improvements

--- src/gif_load_store.c
+++ src/gif_load_store.c
@@ -213,7 +213,7 @@ int output_modified_read_gif(char *filename, GifFileType *g)
 
 int store_pixels(char *filename, animated_gif *image)
 {
-    int n_colors = 0;
+    int n_colors = 256;
     pgrey **p;
     int i, j, k;
     GifColorType *colormap;
@@ -227,40 +227,14 @@ int store_pixels(char *filename, animated_gif *image)
         return 0;
     }
 
-    /* Everything is white by default */
-    for (i = 0; i < 256; i++)
-    {
-        colormap[i].Red = 255;
-        colormap[i].Green = 255;
-        colormap[i].Blue = 255;
+    // we only have 2 colors in the resulting gif
+    for(i = 0; i < 256; i++){
+        colormap[i].Red = i;
+        colormap[i].Green = i;
+        colormap[i].Blue = i;
     }
 
-    /* Change the background color and store it */
-    int moy;
-    moy = (image->g->SColorMap->Colors[image->g->SBackGroundColor].Red +
-           (int) image->g->SColorMap->Colors[image->g->SBackGroundColor].Green +
-           (int) image->g->SColorMap->Colors[image->g->SBackGroundColor].Blue) /
-          3;
-    if (moy < 0)
-        moy = 0;
-    if (moy > 255)
-        moy = 255;
-
-#if SOBELF_DEBUG
-    printf("[DEBUG] Background color (%d,%d,%d) -> (%d,%d,%d)\n",
-           image->g->SColorMap->Colors[image->g->SBackGroundColor].Red,
-           image->g->SColorMap->Colors[image->g->SBackGroundColor].Green,
-           image->g->SColorMap->Colors[image->g->SBackGroundColor].Blue,
-           moy, moy, moy);
-#endif
-
-    colormap[0].Red = moy;
-    colormap[0].Green = moy;
-    colormap[0].Blue = moy;
-
-    image->g->SBackGroundColor = 0;
-
-    n_colors++;
+    image->g->SBackGroundColor = 128;
 
     /* Process extension blocks in main structure */
 
@@ -271,74 +245,7 @@ int store_pixels(char *filename, animated_gif *image)
         f = image->g->ExtensionBlocks[j].Function;
         if (f == GRAPHICS_EXT_FUNC_CODE)
         {
-            int tr_color = image->g->ExtensionBlocks[j].Bytes[3];
-
-            if (tr_color >= 0 &&
-                tr_color < 255)
-            {
-
-                int found = -1;
-
-                moy =
-                    (image->g->SColorMap->Colors[tr_color].Red +
-                     (int) image->g->SColorMap->Colors[tr_color].Green +
-                     (int) image->g->SColorMap->Colors[tr_color].Blue) /
-                    3;
-                if (moy < 0)
-                    moy = 0;
-                if (moy > 255)
-                    moy = 255;
-
-#if SOBELF_DEBUG
-                printf("[DEBUG] Transparency color image %d (%d,%d,%d) -> (%d,%d,%d)\n",
-                       i,
-                       image->g->SColorMap->Colors[tr_color].Red,
-                       image->g->SColorMap->Colors[tr_color].Green,
-                       image->g->SColorMap->Colors[tr_color].Blue,
-                       moy, moy, moy);
-#endif
-
-                for (k = 0; k < n_colors; k++)
-                {
-                    if (
-                        moy == colormap[k].Red &&
-                        moy == colormap[k].Green &&
-                        moy == colormap[k].Blue)
-                    {
-                        found = k;
-                    }
-                }
-                if (found == -1)
-                {
-                    if (n_colors >= 256)
-                    {
-                        fprintf(stderr,
-                                "Error: Found too many colors inside the image\n");
-                        return 0;
-                    }
-
-#if SOBELF_DEBUG
-                    printf("[DEBUG]\tNew color %d\n",
-                           n_colors);
-#endif
-
-                    colormap[n_colors].Red = moy;
-                    colormap[n_colors].Green = moy;
-                    colormap[n_colors].Blue = moy;
-
-                    image->g->ExtensionBlocks[j].Bytes[3] = n_colors;
-
-                    n_colors++;
-                }
-                else
-                {
-#if SOBELF_DEBUG
-                    printf("[DEBUG]\tFound existing color %d\n",
-                           found);
-#endif
-                    image->g->ExtensionBlocks[j].Bytes[3] = found;
-                }
-            }
+            image->g->ExtensionBlocks[j].Bytes[3] = 128;
         }
     }
 
@@ -351,142 +258,12 @@ int store_pixels(char *filename, animated_gif *image)
             f = image->g->SavedImages[i].ExtensionBlocks[j].Function;
             if (f == GRAPHICS_EXT_FUNC_CODE)
             {
-                int tr_color = image->g->SavedImages[i].ExtensionBlocks[j].Bytes[3];
-
-                if (tr_color >= 0 &&
-                    tr_color < 255)
-                {
-
-                    int found = -1;
-
-                    moy =
-                        (image->g->SColorMap->Colors[tr_color].Red +
-                         (int)image->g->SColorMap->Colors[tr_color].Green +
-                         (int)image->g->SColorMap->Colors[tr_color].Blue) /
-                        3;
-                    if (moy < 0)
-                        moy = 0;
-                    if (moy > 255)
-                        moy = 255;
-
-#if SOBELF_DEBUG
-                    printf("[DEBUG] Transparency color image %d (%d,%d,%d) -> (%d,%d,%d)\n",
-                           i,
-                           image->g->SColorMap->Colors[tr_color].Red,
-                           image->g->SColorMap->Colors[tr_color].Green,
-                           image->g->SColorMap->Colors[tr_color].Blue,
-                           moy, moy, moy);
-#endif
-
-                    for (k = 0; k < n_colors; k++)
-                    {
-                        if (
-                            moy == colormap[k].Red &&
-                            moy == colormap[k].Green &&
-                            moy == colormap[k].Blue)
-                        {
-                            found = k;
-                        }
-                    }
-                    if (found == -1)
-                    {
-                        if (n_colors >= 256)
-                        {
-                            fprintf(stderr,
-                                    "Error: Found too many colors inside the image\n");
-                            return 0;
-                        }
-
-#if SOBELF_DEBUG
-                        printf("[DEBUG]\tNew color %d\n",
-                               n_colors);
-#endif
-
-                        colormap[n_colors].Red = moy;
-                        colormap[n_colors].Green = moy;
-                        colormap[n_colors].Blue = moy;
-
-                        image->g->SavedImages[i].ExtensionBlocks[j].Bytes[3] = n_colors;
-
-                        n_colors++;
-                    }
-                    else
-                    {
-#if SOBELF_DEBUG
-                        printf("[DEBUG]\tFound existing color %d\n",
-                               found);
-#endif
-                        image->g->SavedImages[i].ExtensionBlocks[j].Bytes[3] = found;
-                    }
-                }
+                image->g->SavedImages[i].ExtensionBlocks[j].Bytes[3] = 128;
             }
         }
     }
-
-#if SOBELF_DEBUG
-    printf("[DEBUG] Number of colors after background and transparency: %d\n",
-           n_colors);
-#endif
 
     p = image->p;
-
-    /* Find the number of colors inside the image */
-    for (i = 0; i < image->n_images; i++)
-    {
-
-#if SOBELF_DEBUG
-        printf("OUTPUT: Processing image %d (total of %d images) -> %d x %d\n",
-               i, image->n_images, image->width[i], image->height[i]);
-#endif
-
-        for (j = 0; j < image->width[i] * image->height[i]; j++)
-        {
-            int found = 0;
-            for (k = 0; k < n_colors; k++)
-            {
-                if (p[i][j] == colormap[k].Red &&
-                    p[i][j] == colormap[k].Green &&
-                    p[i][j] == colormap[k].Blue)
-                {
-                    found = 1;
-                }
-            }
-
-            if (found == 0)
-            {
-                if (n_colors >= 256)
-                {
-                    fprintf(stderr,
-                            "Error: Found too many colors inside the image\n");
-                    return 0;
-                }
-
-#if SOBELF_DEBUG
-                printf("[DEBUG] Found new %d color (%d,%d,%d)\n",
-                       n_colors, p[i][j].r, p[i][j].g, p[i][j].b);
-#endif
-
-                colormap[n_colors].Red = p[i][j];
-                colormap[n_colors].Green = p[i][j];
-                colormap[n_colors].Blue = p[i][j];
-                n_colors++;
-            }
-        }
-    }
-
-#if SOBELF_DEBUG
-    printf("OUTPUT: found %d color(s)\n", n_colors);
-#endif
-
-    /* Round up to a power of 2 */
-    if (n_colors != (1 << GifBitSize(n_colors)))
-    {
-        n_colors = (1 << GifBitSize(n_colors));
-    }
-
-#if SOBELF_DEBUG
-    printf("OUTPUT: Rounding up to %d color(s)\n", n_colors);
-#endif
 
     /* Change the color map inside the animated gif */
     ColorMapObject *cmo;
@@ -506,25 +283,7 @@ int store_pixels(char *filename, animated_gif *image)
     {
         for (j = 0; j < image->width[i] * image->height[i]; j++)
         {
-            int found_index = -1;
-            for (k = 0; k < n_colors; k++)
-            {
-                if (p[i][j] == image->g->SColorMap->Colors[k].Red &&
-                    p[i][j] == image->g->SColorMap->Colors[k].Green &&
-                    p[i][j] == image->g->SColorMap->Colors[k].Blue)
-                {
-                    found_index = k;
-                }
-            }
-
-            if (found_index == -1)
-            {
-                fprintf(stderr,
-                        "Error: Unable to find a pixel in the color map\n");
-                return 0;
-            }
-
-            image->g->SavedImages[i].RasterBits[j] = found_index;
+            image->g->SavedImages[i].RasterBits[j] = p[i][j];
         }
     }
 
